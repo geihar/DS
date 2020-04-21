c23744a3a2c8eb7fdb488ebdaeb482bae45ebccf
geihar <l0635968488@gmail.com>
2020-04-21 22:43:42 +0200

    first add movies detail

--- movies/views.py
+++ movies/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.views.generic.base import View
 
 from .models import Movie
 
 
-class MovieViews(View):
-    def get(self, request):
-        movie = Movie.objects.all()
-        return render(request, 'movies/movies.html', {'movie_list': movie})
+class MovieViews(ListView):
+
+    model = Movie
+    qs = Movie.objects.filter(draft=False)
 
 
-class MovieDetailView(View):
-
-    def get(self, request, slug):
-        movie = Movie.objects.get(url=slug)
-        return render(request, 'movies/moviesingle.html', {'movie_list': movie})
+class MovieDetailView(DetailView):
+    model = Movie
+    slug_field = 'url'
